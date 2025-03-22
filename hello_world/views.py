79f67ebf0696945572c227afade7b2cdb426f85b
vdi-terminal <vdi-terminal@EDU-WSB.local>
2025-03-22 17:19:15 +0100

    Poprawka na imię 2

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,7 +3,7 @@ from hello_world.formater import get_formatted
 from hello_world.formater import SUPPORTED, PLAIN
 from flask import request
 
-moje_imie = "Natalia"
+moje_imie = "Joachim"
 msg = "Hello World!"
 
 @app.route('/')
@@ -11,7 +11,7 @@ def index():
     output = request.args.get('output')
     if not output:
         output = PLAIN
-    return get_formatted(msg, Joachim,
+    return get_formatted(msg, moje_imie,
                          output.lower())
 
 @app.route('/outputs')
